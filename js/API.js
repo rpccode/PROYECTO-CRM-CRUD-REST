e154164ac479758fe8103733dd6d96b0a26e4f77
Rudy Casilla <rpccode@gmail.com>
2022-01-29 20:50:33 -0500

    Obtener Cli Editar y Guardar Cambios del Cli a Editar
    Co-Authored-By: Rudy Casilla <rpccode@users.noreply.github.com>

--- js/API.js
+++ js/API.js
@@ -31,6 +31,30 @@ export const obtenerClientes = async () =>{
       console.log(e);
   }
 }
+export const obtenerCliente = async id => {
+    try {
+        const resultado = await fetch(`${url}/${id}`);
+        const cliente = await resultado.json();
+        return cliente;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const editarCliente = async cliente => {
+    try {
+        await fetch(`${url}/${cliente.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(cliente), // data puede ser string o un objeto
+            headers:{
+              'Content-Type': 'application/json' // Y le decimos que los datos se enviaran como JSON
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 export const eliminarCliente = async id => {
     try {
